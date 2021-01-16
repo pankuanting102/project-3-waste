01650fa53f9698c67039ffd6370a4a208ca1f45b
Sarah Myriam <sarahmyriam@Sarahs-MacBook-Pro.local>
2021-01-16 13:44:56 -0500

    created a dashboard file and worked on the dashboard

---
+++ client/src/pages/Dashboard.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+const Dashboard = () =>{
+
+    return (
+        
+    
+            <h1>
+                Hello xxx!
+            </h1>
+            <div>for points </div>
+
+            <h5>Share with friends</h5>
+
+            <button  class="btn btn-info">
+                <a href="https://www.facebook.com/">
+                Facebook
+                </a>
+            </button>
+
+            <button  class="btn btn-info">
+                <a href="https://twitter.com/"></a>
+                Twitter
+            </button>
+    
+
+    )
+
+};
+
+
+export default Dashboard;
