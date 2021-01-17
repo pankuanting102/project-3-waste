fb21d9290f86a45ac1eb52f03fa6b0085f335cc2
allie Lewis <allielewis07@gmail.com>
2021-01-17 08:30:35 -0500

    making score indicator

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -5,25 +5,23 @@ import { Link } from "react-router-dom";
 import { Col, Row } from "../components/Grid";
 import { Table, Tr, Td } from "../components/Table";
 import { ForwardRefInput, FormBtn } from "../components/Form";
+import Scale from "../components/Scale";
 
 function Dashboard({ status, username, highScore, score }) {
 
 	// Setting our component's initial state
 	return (
-		<h1 style={{
-			display: "flex",
-			justifyContent: "center",
-			alignItems: "center",
-			backgroundColor: "grey",
-			fontcolor: "white",
-		}}>Hello,  {username}!</h1>
-
-
-
-
+		<div>
+			<h1 style={{
+				display: "flex",
+				justifyContent: "center",
+				alignItems: "center",
+				backgroundColor: "grey",
+				fontcolor: "white",
+			}}>Hello,  {username}!</h1>
+			<Scale />
+		</div>
 	)
-
-
 }
 
 export default Dashboard;
