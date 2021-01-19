ad855602edc8b9dab94c6f01c3e4f3ff1a63d5ad
allie Lewis <allielewis07@gmail.com>
2021-01-18 20:34:58 -0500

    fixing branch and commits

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,32 +1,27 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
+import DeleteBtn from "../components/DeleteBtn";
+import API from "../utils/API";
+import { Link } from "react-router-dom";
+import { Col, Row } from "../components/Grid";
+import { Table, Tr, Td } from "../components/Table";
+import { ForwardRefInput, FormBtn } from "../components/Form";
+import Scale from "../components/Scale";
 
-const Dashboard = () =>{
+function Dashboard({ status, username, highScore, score }) {
 
+    // Setting our component's initial state
     return (
-        
-    
-            <h1>
-                Hello xxx!
-            </h1>
-            <div>for points </div>
-
-            <h5>Share with friends</h5>
-
-            <button  class="btn btn-info">
-                <a href="https://www.facebook.com/">
-                Facebook
-                </a>
-            </button>
-
-            <button  class="btn btn-info">
-                <a href="https://twitter.com/"></a>
-                Twitter
-            </button>
-    
-
+        <div>
+            <h1 style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                backgroundColor: "grey",
+                fontcolor: "white",
+            }}>Hello,  {username}!</h1>
+            <Scale />
+        </div>
     )
-
-};
-
+}
 
 export default Dashboard;
